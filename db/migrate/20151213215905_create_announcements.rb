bd77ad366670676a57f30e16645bc348fdd1852e
Piotr Pawluś <pawlus.piotrek@gmail.com>
2015-12-14 23:37:04 +0100

    add foreign key, position and visible

--- db/migrate/20151213215905_create_announcements.rb
+++ db/migrate/20151213215905_create_announcements.rb
@@ -1,12 +1,17 @@
 class CreateAnnouncements < ActiveRecord::Migration
   def up
     create_table :announcements do |t|
+      t.integer  "announcements_id"
       t.string "title", :limit => 100, :null => false
+      t.integer "positon"
       t.date "date_of_start", :null => false
       t.integer "price_per_hour", :null => false
       t.integer "price_per_day", :null => true
+      t.boolean "visible", :default=>true
       t.timestamps null: false
     end
+    add_index("announcements", "announcements_id")
+
   end
 
   def down
