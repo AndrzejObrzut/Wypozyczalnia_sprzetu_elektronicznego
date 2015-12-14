8772f27e7f5d9d3c94d6700eb73c140a4fac36a8
Piotr Pawluś <pawlus.piotrek@gmail.com>
2015-12-15 00:33:58 +0100

    AnnouncementCategory scopes

--- app/models/announcement_category.rb
+++ app/models/announcement_category.rb
@@ -1,3 +1,9 @@
 class AnnouncementCategory < ActiveRecord::Base
   has_many :announcements
+
+  scope :visible, lambda{where(:visible => true)}
+  scope :invisible, lambda{where(:visible => false)}
+  scope :sortASC, lambda{order("announcement_categories.position ASC")}
+  scope :sortNew, lambda{order("announcement_categories.created_at DESC")}
+
 end
