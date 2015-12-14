e491112ba58fc49fcb6e9aeef6e5653fbc6b26f6
Piotr Pawluś <pawlus.piotrek@gmail.com>
2015-12-14 23:48:07 +0100

    Created gallerie of photos

---
+++ db/migrate/20151214224350_create_galleries.rb
@@ -0,0 +1,14 @@
+class CreateGalleries < ActiveRecord::Migration
+  def up
+    create_table :galleries do |t|
+      t.string "name", :null=>false
+      t.boolean "visible", :default=>false
+      t.attachment "photo"
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :galleries
+  end
+end
