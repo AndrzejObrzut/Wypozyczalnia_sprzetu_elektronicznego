951bc3166d978f0a3aaae2ad4b5aea45f2f96711
Piotr Pawluś <pawlus.piotrek@gmail.com>
2015-12-17 22:30:29 +0100

    cheange why

--- app/models/announcement_category.rb
+++ app/models/announcement_category.rb
@@ -1,7 +1,5 @@
 class AnnouncementCategory < ActiveRecord::Base
   has_many :announcements
-  scope :visible, lambda{where(:visible => true)}
-  scope :invisible, lambda{where(:visible => false)}
   scope :sortASC, lambda{order("announcement_categories.position ASC")}
   scope :sortNew, lambda{order("announcement_categories.created_at DESC")}
 
