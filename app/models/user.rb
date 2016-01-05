ec0f46bd725c7fb5d840e0020c6f20310ccba7c9
andrzej0221 <andrzejo94@gmail.com>
2016-01-05 23:00:33 +0100

    change email field, begin of validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
-  has_secure_password
 
+  validates :user_name, presence: true
+  validates :password_digest, presence: true
+  validates :first_name, presence: true
+  validates :second_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+
+  has_secure_password
   scope :sortNew, lambda{order("users.created_at DESC")}
 
 end
