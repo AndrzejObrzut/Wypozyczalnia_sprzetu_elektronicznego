26b7b3ed90e8cc4d0a677a4228c1524705953c04
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-05 16:49:09 +0100

    move functions from welcome to access controller

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -6,8 +6,23 @@ class AccessController < ApplicationController
   end
 
   def registration
+    @new_user = User
   end
 
   def users
+    @users = User.sortNew
+  end
+
+  def create
+    @create_new_user = User.new(new_user_parametrs)
+    if @create_new_user.save
+      redirect_to(:action=>'index')
+    else
+      render('registration')
+    end
+  end
+
+  def new_user_parametrs
+    params.require(:create_new_user).permit(:user_name, :password, :password_confirmation, :first_name, :second_name, :email, :phone)
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,34 +7,4 @@ class WelcomeController < ApplicationController
         @random = params[:random]
       end
   end
-
-  #test test
-  def sample
-    @controller_message = "Hello From Welcome Controller"
-  end
-
-  def login
-  end
-
-  def registration
-      @new_user = User
-  end
-
-  def registerd
-      @users = User.sortNew
-  end
-
-  def create
-    @create_new_user = User.new(new_user_parametrs)
-    if @create_new_user.save
-      redirect_to(:action=>'index')
-    else
-      render('registration')
-    end
-  end
-
-  def new_user_parametrs
-    params.require(:create_new_user).permit(:user_name, :password, :password_confirmation, :first_name, :second_name, :email, :phone)
-  end
-
 end
