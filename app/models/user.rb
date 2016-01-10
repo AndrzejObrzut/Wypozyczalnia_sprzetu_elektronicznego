b4728216b5af874da0fd1b7c49971e822709bcca
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-10 22:56:50 +0100

    update validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ActiveRecord::Base
 
-  validates :user_name, presence: true
-  validates :password_digest, presence: true
-  validates :first_name, presence: true
-  validates :second_name, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
+  validates :user_name,
+            uniqueness: true,
+            presence: true
+
+  validates :password_digest,
+            presence: true
+
+  validates :first_name,
+            length: {minimum: 1, maximum: 100},
+            presence: true
+
+  validates :second_name,
+            length: {minimum: 1, maximum: 100},
+            presence: true
+
+  validates :email,
+            uniqueness: true,
+            presence: true
+
+  validates :phone,
+            presence: true
 
   has_secure_password
   scope :sortNew, lambda{order("users.created_at DESC")}
