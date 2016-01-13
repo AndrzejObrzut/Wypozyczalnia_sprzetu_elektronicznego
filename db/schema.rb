0dff6d31e729784326f94ea8fe0b6fbe2d13a2ba
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-13 22:44:31 +0100

    create model, controler and views for photos db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110230057) do
+ActiveRecord::Schema.define(version: 20160113184531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 20160110230057) do
     t.boolean  "visible",    default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.binary   "data",       null: false
+    t.string   "filename"
+    t.string   "mime_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
