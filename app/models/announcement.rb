4daba58ba121cc694e94fabdde45a33eab7cbb26
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-13 17:36:01 +0100

    search in announcment by title

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,20 +4,18 @@ class Announcement < ActiveRecord::Base
 
 
   validates :title,
-            length: {minimum: 2, maximum: 100, :message => "Tytuł musi zawierać od 2 do 100 znaków."},
-            presence: true
+            length: {minimum: 2, maximum: 100, :message => "Tytuł musi zawierać od 2 do 100 znaków."}
 
   validates :description,
-            length: {maximum: 1000, :message => "Opis nie może być dłuższy niż 1000 znaków."},
-            presence: true
+            length: {minimum: 2, maximum: 1000, :message => "Opis musi zawierać od 2 do 1000 znaków."}
 
   validates :price_per_hour,
-            :numericality => {:greater_than => 0},
-            presence: true
+            :numericality => {:greater_than_or_equal_to  => 0, :message => "Cena za godzinę musi być liczbą większą bądź równą 0"}
+
 
   validates :price_per_day,
-            presence: false,
-            :numericality => true
+            :numericality => {:greater_than_or_equal_to => 0, :message => "Cena za dzień musi być liczbą większą bądź równą 0"}
+
 
   scope :sortNew, lambda{order("announcements.created_at DESC")}
 
