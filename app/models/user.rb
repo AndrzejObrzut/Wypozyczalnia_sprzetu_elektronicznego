9c2bf79096d1d15ec48540e6cf6ad6cb5c896751
andrzej0221 <andrzejo94@gmail.com>
2016-01-14 01:47:35 +0100

    validation of registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,27 @@ class User < ActiveRecord::Base
   has_many :announcements
 
   validates :user_name,
-            uniqueness: true,
-            presence: true
+            uniqueness: {:message => "Nazwa użytkownika jest zajęta"},
+            presence: {:message => "Wpisz nazwę użytkownika"}
 
-  validates :password_digest,
-            presence: true
+
+  validates :password,
+            presence: true,
+            confirmation: {:message => "Wpisz hasło i poprawnie go potwierdź"}
+
 
   validates :first_name,
-            length: {minimum: 1, maximum: 100},
-            presence: true
+            length: {minimum: 1, maximum: 100, :message => "Imię musi zawierać od 1 do 100 znaków"}
 
   validates :second_name,
-            length: {minimum: 1, maximum: 100},
-            presence: true
+            length: {minimum: 1, maximum: 100, :message => "Nazwisko musi zawierać od 1 do 100 znaków"}
 
   validates :email,
-            uniqueness: true,
-            presence: true
+            uniqueness: {:message => "Email jest zajęty"},
+            presence: {:message => "Wpisz email"}
 
   validates :phone,
-            presence: true
+            presence: {:message => "Wpisz numer telefonu"}
 
   has_secure_password
   scope :sortNew, lambda{order("users.created_at DESC")}
