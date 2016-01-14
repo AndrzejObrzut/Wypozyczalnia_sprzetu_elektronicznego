d9922759c4615eeab41dd8f66a78c82833cdbd51
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-14 21:11:46 +0100

    Correct insert data to db, display image from db, download image from db

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+
+  @@test = ""
   def index
     @photos = Photo.all
   end
@@ -13,11 +15,20 @@ class PhotosController < ApplicationController
     #               :type => @photo.mime_type,
     #               :filename => @photo.filename,
     #               :disposition => "inline")
-
+    #
+    # puts @photo.mime_type, "===="
+    # puts @photo.data.class, "===="
     send_data @photo.data,
               type: @photo.mime_type,
               filename: @photo.filename,
-              disposition:"inline"
+              disposition: "attachment"
+
+  end
+
+
+  def download
+    @photo = Photo.find params[:id]
+    send_data @photo.data, filename: @photo.filename, type: @photo.mime_type, disposition: 'attachment'
   end
 
   def new
@@ -25,11 +36,14 @@ class PhotosController < ApplicationController
   end
 
   def create
-
     # build a photo and pass it into a block to set other attributes
+    # puts photo_params
+    cos = photo_params
+    puts cos, "========"
     @photo = Photo.new(photo_params)
     @photo.update_attributes(:filename => @photo.data.original_filename)
     @photo.update_attributes(:mime_type => @photo.data.content_type)
+    @photo.update_attributes(:data => @photo.data.read)
     @photo.update_attributes(:size => @photo.data.size)
 
     # normal save
