85b86b4173f7775d99360e90640e3898a98ffc40
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-15 17:05:38 +0100

    Change from sencond to last name

--- db/migrate/20151214200330_create_users.rb
+++ db/migrate/20151214200330_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string "user_name", :limit=>30,  :null=>false
       t.string "password_digest"
       t.string "first_name", :limit=>40, :null=>false
-      t.string "second_name", :limit=>40, :null=>false
+      t.string "last_name", :limit=>40, :null=>false
       t.string "email", :limit=>60, :null=>false
       t.string "phone", :null=>false
       t.boolean "is_admin", :default=>false
