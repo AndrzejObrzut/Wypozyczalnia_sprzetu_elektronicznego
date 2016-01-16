85afb31f789cf25c641ba5ec2f1ff25e54d63b3c
andrzej0221 <andrzejo94@gmail.com>
2016-01-16 23:22:21 +0100

    add photos to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+      @photos = Photo.sortOldPhotos
       @categories = AnnouncementCategory.all
       @announcements = Announcement.sortNew
       if(params.has_key?(:id) && params.has_key?(:random))
