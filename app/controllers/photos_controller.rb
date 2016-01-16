d964a893de162158b9c778ff55eb6e13ff8455e1
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-16 11:56:36 +0100

    Add validation off attachment in photo model. New, Edit, Delete for photo.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,23 +9,24 @@ class PhotosController < ApplicationController
   end
 
   def create
-    puts photo_param
-    @photo = Photo.new(photo_param)
+    @photo = Photo.new(photo_parameters)
     if @photo.save
       redirect_to(:action=>'index')
     else
       flash[:notice] = "Błąd w podawaniu danych"
+      @announcements = Announcement.all
       render('new')
     end
   end
 
   def edit
     @photo = Photo.find(params[:id])
+    @announcements = Announcement.all
   end
 
   def update
     @photo = Photo.find(params[:id])
-    if @photo.update_attributes(categories_parameters)
+    if @photo.update_attributes(photo_parameters)
       redirect_to(:action=>'index')
     else
       render('edit')
@@ -34,7 +35,6 @@ class PhotosController < ApplicationController
 
   def show
   end
-
 
   def delete
     @photo = Photo.find(params[:id])
@@ -45,7 +45,8 @@ class PhotosController < ApplicationController
     redirect_to(:action => 'index')
   end
 
-  def photo_param
-    params.require(:photo).permit(:announcement_id)
+  def photo_parameters
+    params.require(:photo).permit(:announcement_id, :image)
   end
+
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,16 @@
 class Photo < ActiveRecord::Base
   belongs_to :announcement
 
+  has_attached_file :image,
+                    :styles => { :medium => "600x600>", :thumb => "200x200>" }
+
+  validates_attachment_content_type :image,
+                                    :content_type => /^image\/(png|jpeg)/,
+                                    :message => "Złe rozszerzenie. Akceptuje .png lub .jpeg."
+
+  validates_attachment_size :image,
+                            :in => 0..1.megabyte,
+                            :message => "Plik za duży. Maksymalnie 1 MB."
+
   scope :sortOldPhotos, lambda{order("photos.created_at ASC")}
 end
